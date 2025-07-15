6abe85feae26a0f2e0398004694c3f1b50602b33
Maheswari571 <maheswari.arumugamsamy@gmail.com>
2025-07-15 13:36:58 +0530

    Add files via upload

---
+++ OddOrEven.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+public class OddOrEven {
+	public static void main(String[] args) {
+		int num=10;
+		for(int i=0;i<num;i++) {
+			if(i%2==0) {
+				System.out.println("Even Numbers: "i);
+			}
+			else {
+				System.out.println("Odd Numbers: "i);
+			}
+		}
+	}
+
+}
